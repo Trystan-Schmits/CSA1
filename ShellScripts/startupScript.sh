f0412756587a477046b19ad67dd8c4075a1b0da5
Trystan-Schmits <142453909+Trystan-Schmits@users.noreply.github.com>
2024-08-30 12:25:59 -0700

    Update startupScript.sh

--- ShellScripts/startupScript.sh
+++ ShellScripts/startupScript.sh
@@ -1,1 +1,29 @@
+#! /bin/bash
 
+echo starting venv setup
+cd
+cd vscode/portfolio_2025
+scripts/venv.sh
+source venv/bin/activate
+
+echo venv setup finished
+
+echo starting version checks
+
+# Setup Python libraries for Notebook conversion
+pip install nbconvert  # library for notebook conversion
+pip install nbformat  # notebook file utility
+pip install pyyaml  # notebook frontmatter
+
+#show versions
+# Show the active Ruby version, it needs to be 3 or higher
+    ruby -v
+# Bundler version, it is part of Ruby install
+    bundle -v
+
+# Show active Python version, it needs to be 3.10 or better (mine should currently be 3.10.12)
+    python --version
+
+# Show Jupyter packages, nbconvert needs to be in the list of installed
+    jupyter --version
+echo version checks finished
